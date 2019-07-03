65abf27f1a878f7df0ae1e4b5ba43ad53480605b
mihail1986 <yahim09@gmail.com>
2019-07-03 18:07:16 +0300

    Initial commit

---
+++ .history/src/components/app_20190701181333.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Cepraga Mihail Portfolio</h1>
+        <h2>React Redux Router</h2>
+      </div>
+    );
+  }
+}
---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Cepraga Mihail Portfolio</h1>
+        <h2>React Redux Router</h2>
+        <h3>
+          {moment().format('MMMM Do YYYY, h:mm:ss a')}
+        </h3>
+      </div>
+    );
+  }
+}

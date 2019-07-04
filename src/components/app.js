8b093bac33b4caf295c8c5ba514b384d86099faf
mihail1986 <yahim09@gmail.com>
2019-07-04 20:16:28 +0300

    implement react route

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import moment from "moment";
-import PortfolioContainer from './portfolio/portfolio-container'
-import NavigationContainer from './navigation/navigation-container'
+import moment from 'moment';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+
+      <Router>
+        <div>
         <NavigationContainer />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about-me" component={About} />
+        </Switch>
+        </div>
+      </Router>
+
         <h1>Cepraga Mihail Portfolio</h1>
         <h2>React Redux Router</h2>
         <h3>

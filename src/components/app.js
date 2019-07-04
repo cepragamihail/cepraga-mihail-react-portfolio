b6ee5b3312c3d7786cbd4dfa227458f79753863d
mihail1986 <yahim09@gmail.com>
2019-07-04 19:54:00 +0300

    add navigation component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import moment from "moment";
 import PortfolioContainer from './portfolio/portfolio-container'
+import NavigationContainer from './navigation/navigation-container'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Cepraga Mihail Portfolio</h1>
         <h2>React Redux Router</h2>
         <h3>

e66654704c4b1e271f49a8bbe462ab58ecedd517
mihail1986 <yahim09@gmail.com>
2019-07-05 19:42:39 +0300

    implement axios

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import axios from 'axios';
 
 import NavigationContainer from './navigation/navigation-container';
 import Home from './pages/home';
@@ -15,28 +16,49 @@ import NoMatch from './pages/no-match';
 import moment from 'moment';
 
 export default class App extends Component {
+
+  constructor() {
+    super();
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems() {
+    // Make a request for a user with a given ID
+    axios.get('https://cepragamihail.devcamp.space/portfolio/portfolio_items')
+      .then( response => {
+        console.log("my respons data:=> ", response);
+      })
+      .catch( error => {
+        console.log(error);
+      })
+      .finally( () => {
+        console.log('finally method')
+      });
+  }
+
   render() {
+    this.getPortfolioItems();
     return (
       <div className='app'>
 
-      <Router>
-        <div>
-        <h1>Cepraga Mihail Portfolio</h1>
-        <h3>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </h3>
-        <NavigationContainer />
+        <Router>
+          <div>
+            <h1>Cepraga Mihail Portfolio</h1>
+            <h3>
+              {moment().format('MMMM Do YYYY, h:mm:ss a')}
+            </h3>
+            <NavigationContainer />
 
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about-me" component={About} />
-          <Route path="/contact" component={Contcat} />
-          <Route path="/blog" component={Blog} />
-          <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
-          <Route component={NoMatch} />
-        </Switch>
-        </div>
-      </Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contcat} />
+              <Route path="/blog" component={Blog} />
+              <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
+            </Switch>
+          </div>
+        </Router>
 
       </div>
     );

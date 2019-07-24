f9ed001d2296773b688ce97921a7bc7a42b8617b
mihail1986 <yahim09@gmail.com>
2019-07-24 17:54:39 +0300

    implement delete protfolio item functionality

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -15,6 +15,23 @@ export default class PortfolioManager extends Component {
         
         this.handleSuccessfulFromSubmission = this.handleSuccessfulFromSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+        this.handleDeleteClick = this.handleDeleteClick.bind(this);
+    }
+
+    handleDeleteClick(portfolioItem) {
+        axios.delete(
+            `https://api.devcamp.space/portfolio/portfolio_items/${portfolioItem.id}`,
+            { withCredentials: true }
+        ).then(response => {
+            this.setState({
+                portfolioItems: this.state.portfolioItems.filter(
+                    item => item.id !== portfolioItem.id
+                )
+            });
+            return response.data;
+        }).catch(error => {
+            console.log("handleDeleteClick error", error);
+        })
     }
     
 handleSuccessfulFromSubmission(portfolioItem) {
@@ -53,7 +70,10 @@ handleFormSubmissionError(error) {
                     />
                 </div>
                 <div className="right-column">
-                    <PortfolioSidebarList data={this.state.portfolioItems}/>
+                    <PortfolioSidebarList 
+                    handleDeleteClick={this.handleDeleteClick}
+                    data={this.state.portfolioItems}
+                    />
                 </div>
             </div>
         );
